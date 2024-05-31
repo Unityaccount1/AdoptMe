df30de1852343c32891e56ea1ac9af996ec42ab2
Hanz <67298719+Unityaccount1@users.noreply.github.com>
2024-05-31 04:04:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+from time import time
+from typing import List, Optional
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+
+class InputJsonConversation(BaseModel):
+    id: Optional[int] = None
+    message: str = None
+    
+    
+class InputJsonImage(BaseModel):
+    id: Optional[int] = None
+    fileBase64: str
+    description: str
+            
+
+class OutputJsonImage(InputJsonImage):
+    pass
+
+class OutputJsonUploadImage(InputJsonConversation):
+    pass
+    
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+jsonObjectImages=[]
+
+@app.middleware("http")
+async def log_middleware(request, call_next):
+    start_time = time()
+    response = await call_next(request)
+    end_time = time()
+    process_time = end_time - start_time
+    print(f"Request {request.url} proccesed in {process_time} in sec")
+    return response
+
+@app.post("/v1/uploadImage")
+async def uploadImage(InputImage: InputJsonImage):
+    InputImage.id = len(jsonObjectImages) + 1
+    jsonObjectImages.append(InputImage)
+    message = "Imagen cargada correctamente"
+    return message
+    
+@app.post("/v1/getResponse")
+async def uploadImage(InputConversation: InputJsonConversation):
+    message = "Mostrando el total de imagenes cargadas"
+    return jsonObjectImages
